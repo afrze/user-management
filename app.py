1f5d6a8a52d11e825df4019e6d50d68e66745846
afrze <afroze@outlook.in>
2023-06-19 16:02:53 +0530

    completed update adn delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,9 +17,46 @@ class Users(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-      pass
+      from_user_input = request.form['user']
+      new_user =  Users(name=from_user_input) 
+      
+      try:
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+      except:
+        return "There was an issues adding the user"
+    
     else:
-      return render_template('index.html')
+      users = Users.query.order_by(Users.date_created).all()
+      return render_template('index.html', users=users)
+    
+@app.route('/delete/<int:id>')
+def deleteUser(id):
+  user_to_delete = Users.query.get_or_404(id)
+  try:
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return redirect('/')
+  except:
+    return 'There was an issue deleting the user'
+  
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def updateUser(id):
+  user_to_update = Users.query.get_or_404(id)
+
+  if request.method == 'POST':
+    user_to_update.name = request.form['user']
+
+    try:
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was an issue updating the user'
+
+  else: 
+    return render_template('update.html', user=user_to_update)
 
 if __name__ == "__main__":
   app.run(debug=True)
